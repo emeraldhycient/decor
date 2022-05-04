762d46fed596a27e2d8bc9d80850dd20935e23f2
emeraldhycient <igwezehycient86@gmail.com>
2022-05-04 13:50:09 +0100

    getServerSideProps no gree work

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   const year = d.getFullYear();
   return (
     <div className=" h-[100vh] mt-20">
-      <div className="h-3/6 w-full bg-gray-200 pt-16 md:pt-10 ">
+      <div className="h-fit md:h-3/6 w-full bg-gray-200 pt-16 md:pt-10 pb-8 ">
         <div className="md:w-3/5 mx-3	md:mx-auto text-center">
           <h1 className="text-4xl font-medium text-gray-500">
             Subscribe To Get The Latest
